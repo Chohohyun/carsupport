0002597c3b270ca567b7c8acab97cb0e2c835512
chochohyun <chohohyun@naver.com>
2019-07-05 10:13:18 +0900

    asdasdasad

--- src/main/java/com/support/movement/car/LoginController.java
+++ src/main/java/com/support/movement/car/LoginController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 // 깃허브 테스트합니다.
+// 기섷브 테스트 2
 // 가상 URL 주소로 접속하면 호출되는 메소드를 소유한 컨트롤러 클래스 선언
 // @Controller 를 붙임으로써 컨트롤러 클래스임을 지정한다.
 @Controller
